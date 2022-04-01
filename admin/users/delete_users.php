12254fe53a2ae305fa04654dd8ace32db926ecb7
Sam-Nyalik <samnyalik@gmail.com>
2022-04-01 12:55:51 +0300

    User deletion from the system

---
+++ admin/users/delete_users.php
@@ -0,0 +1,24 @@
+<?php 
+
+// Start session
+session_start();
+
+// Include the database
+include_once "functions/functions.php";
+$pdo = databaseConnect();
+
+//Prepare a DELETE statement from the database specifying the id in the URL
+$sql = ("DELETE FROM users WHERE id = :id");
+if($stmt = $pdo->prepare($sql)){
+    // Bind the variables as parameters
+    $stmt->bindParam(":id", $param_id, PDO::PARAM_INT);
+    //Set parameters
+    $param_id = $_GET['id'];
+    // Attempt to execute
+    if($stmt->execute()){
+        header("location: index.php?page=admin/dashboard");
+    } else {
+        echo "<script>alert('There was an eror deleting the user. Please try again!');</script>";
+    }
+
+}
