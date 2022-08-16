85afd62e0a20364d175e8c45955e50a01f4dfc18
Sam-Nyalik <samnyalik@gmail.com>
2022-08-16 13:39:26 +0300

    Quick update

--- functions/functions.php
+++ functions/functions.php
@@ -7,7 +7,7 @@ function databaseConnect()
     $DATABASE_HOST = "localhost";
     $DATABASE_USER = "root";
     $DATABASE_PASSWORD = "";
-    $DATABASE_NAME = "house_rental_system";
+    $DATABASE_NAME = "house-rental-system";
 
     try {
         return new PDO("mysql:host=" . $DATABASE_HOST . ";dbname=" . $DATABASE_NAME . ";charset=utf8", $DATABASE_USER, $DATABASE_PASSWORD);
@@ -41,6 +41,9 @@ function header_template($title)
         <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
         <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
         <link href=\"https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap\" rel=\"stylesheet\"> 
+        <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
+        <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
+        <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap\" rel=\"stylesheet\"> 
         <link rel=\"icon\" href=\"images/image1.jpg\" sizes=\"16*16\">
         </head>
     ";
